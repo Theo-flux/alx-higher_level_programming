eafb34bf8272b935e8572f944b53ccdcdcc20341
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-28 03:46:00 -0700

    Hexadecimal printing

--- 0x01-python-if_else_loops-functions/4-print_hexa.py
+++ 0x01-python-if_else_loops-functions/4-print_hexa.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99, 1):
-    if (i < 10):
-        print("{} = 0x{}".format(i, i))
-    elif(i > 9):
-        if(i % 16 == 0):
-           print("{} = 0x{:0.0f}0".format(i, i/16))
-        elif(i % 16 > 9 and i % 16 < 16):
-            if (i % 16 == 10):
-                print("{} = 0x{}a".format(i, i/16))
-            elif(i % 16 == 11):
-                print("{} = 0x{}b".format(i, i/16))
+for i in range(0, 99):
+    print("{:d} = 0x{:x}".format(i, i))
 
