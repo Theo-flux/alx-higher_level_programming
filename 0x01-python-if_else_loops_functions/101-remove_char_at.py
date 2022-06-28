2362305067a4b1a9b044f5d5530d3a74f69b4982
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-28 15:56:56 -0700

    remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bash/python3
+def remove_char_at(str, n):
+    if (n > len(str)):
+        return str
+    else:
+        listStr = list(str)
+        listStr.pop(n)
+        str= "".join(listStr)
+        return str
