150430fd5f2d5eb495bb812e8080d4f664c79763
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-28 07:31:53 -0700

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,4 @@ for i in range(0, 100):
     if(i < 99):
         print("{:02d}, ".format(i), end="")
     else:
-        print("{:d}".format(i))
-print("\n")
+        print("{:d}".format(i), end="\n")
