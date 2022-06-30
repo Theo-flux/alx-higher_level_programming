d461e454beef85b333e084947cb3ee6a5de20993
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-30 04:09:21 -0700

    2-args.py hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-argv.pop(0)
-argvLen = len(argv)
-i = 0
-if argvLen != 0:
-    if (argvLen == 1):
-        print("{:d} argument:".format(argvLen), end="\n")
+if __name__ == "__main__":
+    from sys import argv
+    argv.pop(0)
+    argvLen = len(argv)
+    i = 0
+    if argvLen != 0:
+        if (argvLen == 1):
+            print("{:d} argument:".format(argvLen), end="\n")
+        else:
+            print("{:d} arguments:".format(argvLen), end="\n")
+        while i < argvLen:
+            print("{:d}: {}".format(i, argv[i]), end="\n")
+            i = i + 1
     else:
-        print("{:d} arguments:".format(argvLen), end="\n")
-    while i < argvLen:
-        print("{:d}: {}".format(i, argv[i]), end="\n")
-        i = i + 1
-else:
-    print("{:d} arguments.".format(argvLen), end="\n")
+        print("{:d} arguments.".format(argvLen), end="\n")
