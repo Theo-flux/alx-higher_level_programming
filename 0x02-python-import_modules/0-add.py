4e8d867cb849e7086a840c3236bad6e20f159af9
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-30 04:34:46 -0700

    doc string added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Importsfunction def add(a, b) from add_0.py
+    print the result of the addition 1 + 2 = 3
+    """
     from add_0 import add
     a = 1
     b = 2
