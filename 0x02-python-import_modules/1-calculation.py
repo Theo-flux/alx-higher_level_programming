9271de02f2ef1586868f9a252970d7df8e216995
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-30 06:44:07 -0700

    refactored

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+    
     a = 10
     b = 5
-    res= 0
-    res = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, res), end="\n")
-    res = sub(a, b)
-    print("{:d} - {:d} = {:d}".format(a, b, res), end="\n")
-    res = mul(a, b)
-    print("{:d} * {:d} = {:d}".format(a, b, res), end="\n")
-    res = div(a, b)
-    print("{:d} / {:d} = {:d}".format(a, b, res), end="\n")
+    
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)), end="\n")
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)), end="\n")
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)), end="\n")
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)), end="\n")
