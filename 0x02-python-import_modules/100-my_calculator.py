d70d9436dd20927bab32b6ead377fbfeb4718baf
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-30 08:42:22 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     size = len(argv) - 1
-
     if (size != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>", end="\n")
         exit(1)
@@ -12,7 +11,6 @@ if __name__ == "__main__":
         operator = {'+':add, '-':sub, '*':mul, '/':div}
         a = int(argv[1])
         b = int(argv[3])
-
         if argv[2] in list(operator.keys()):
             print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], operator[argv[2]](a, b)), end="\n")
         else:
