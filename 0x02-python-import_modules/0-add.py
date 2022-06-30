a65324cf70330f579ef6b4ddfb236d7521909660
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-30 06:40:28 -0700

    refcatored

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-
 if __name__ == "__main__":
     """Print 1 + 2 = 3"""
     from add_0 import add
     
     a = 1
     b = 2
-    res = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a,b,res), end="\n")
+    print("{:d} + {:d} = {:d}".format(a,b,add(a, b)), end="\n")
