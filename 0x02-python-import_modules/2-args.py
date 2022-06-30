189f0380a61178bc5ec7d1f00bae3f081b92f5cd
Theo Phil <ekunnusitheophilus@gmail.com>
2022-06-30 11:33:32 -0700

    refactored 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from sys import argv
-    argv.pop(0)
-    argvLen = len(argv)
-    i = 0
-    if argvLen != 0:
-        if (argvLen == 1):
-            print("{:d} argument:".format(argvLen), end="\n")
-        else:
-            print("{:d} arguments:".format(argvLen), end="\n")
-        while i < argvLen:
-            print("{:d}: {}".format(i, argv[i]), end="\n")
-            i = i + 1
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{:d} arguments.".format(argvLen), end="\n")
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
