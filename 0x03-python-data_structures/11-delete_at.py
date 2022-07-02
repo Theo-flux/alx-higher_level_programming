a7f9a23c26ea1b7e80bc0f1b49092b03fcf26575
Theo-flux <72856939+Theo-flux@users.noreply.github.com>
2022-07-02 10:09:11 +0100

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,7 @@
 #!/bin/usr/python3
 def delete_at(my_list=[], idx=0):
-    if my_list == []:
-        return None
+    if (idx < 0 or idx >= len(my_list)):
+        return my_list
     else:
-        if (idx < 0 or idx >= len(my_list)):
-            return my_list
-        else:
-            del my_list[idx]
-            return my_list
+        del my_list[idx]
+        return my_list
