4b74f303e9eb06a5ec527441a5a382c065d51ff9
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-01 20:43:17 -0700

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list == []:
+        return None
+    else:
+        i = 0
+        new_list = []
+        while i < len(my_list):
+            if my_list[i] % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+            i = i + 1
+        
+        return new_list
