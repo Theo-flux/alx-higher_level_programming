ee1aacd24c20cf4f7813f74821b8a4745d7137ec
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-01 20:49:02 -0700

    Delete at

---
+++ 0x03-python-data_structures/11-delete.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def delete_at(my_list=[], idx=0):
+    if my_list = []:
+        return None
+    else:
+        if (idx < 0 or idx >= len(my_list)):
+            return my_list
+        else:
+            del my_list[idx]
+            return my_list
