fa9f39ec5be8f939de25596793d78f7619423a8a
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-04 15:16:15 -0700

    0-print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    listLen = len(my_list)
-    if (listLen != 0):
-        for el in my_list:
+    for el in my_list:
             print("{:d}".format(el))
 
