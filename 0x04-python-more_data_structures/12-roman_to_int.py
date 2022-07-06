52bdacf4f1fb01dcbd3024cf0cd240d78e58e90c
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-06 12:35:01 -0700

    hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not roman_string or type(roman_string) != str):
-        return None
-    else:
-        myHash = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
-        integer = 0
-        for i in range(len(roman_string)):
-            if i+1 < len(roman_string) and myHash[roman_string[i]] < myHash[roman_string[i+1]]:
-                integer -= myHash[roman_string[i]]
-            else:
-                integer += myHash[roman_string[i]]
-        return integer
+    if not roman_string or type(roman_string) != str:
+        return 0
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
+        else:
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
