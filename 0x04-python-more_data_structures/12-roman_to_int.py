faeebdc2b15b4790613e18103581156ed69d8b8f
Theo Phil <ekunnusitheophilus@gmail.com>
2022-07-06 10:07:41 -0700

    roman-to-integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    myHash = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+
+    integer = 0
+
+    for i in range(len(roman_string)):
+        if i+1 < len(roman_string) and myHash[roman_string[i]] < myHash[roman_string[i+1]]:
+            integer -= myHash[roman_string[i]]
+        else:
+            integer += myHash[roman_string[i]]
+    return integer
