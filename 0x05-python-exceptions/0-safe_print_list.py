542de1385b44ded0ba4248b965324629679bc179
Theo-flux <ekunnusitheophilus@gmail.com>
2022-07-18 11:29:47 +0000

    ...

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,11 +6,11 @@ def safe_print_list(my_list=[], x=0):
         try:
             i = 0
             lst = []
-            while (i < x):
+            while (i <= x):
                 print(my_list[i], end="")
                 i = i + 1
             print("", end="\n")
-        except IndexError as ex:
+        except IndexError:
             pass
         finally:
             return(i)
