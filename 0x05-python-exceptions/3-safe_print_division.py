5909e94faa7f7c83b6219639cd1092f7ce1c24e7
Theo-flux <ekunnusitheophilus@gmail.com>
2022-07-18 12:16:35 +0000

    zerodivision error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    if (a and b):
-        result = 0
-        try:
-            result = a / b
-        except ZeroDivisionError:
-            result = None
-            return result
-        else:
-            return result
-        finally:
-            print("Inside result: {}".format(result))
-    else:
-        return None
+    try:
+        result = a / b
+    except (ZeroDivisionError, TypeError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
