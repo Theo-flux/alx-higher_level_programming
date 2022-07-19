0af15834758732d90a675c954cd7b8b46bdb5a9e
Theo-flux <ekunnusitheophilus@gmail.com>
2022-07-19 20:15:39 +0000

    task-5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -9,10 +9,8 @@ class Square:
 
     def __init__(self, size=0):
         """An init method for class Square.
-
         Args:
             size (int): An integer varibale.
-
         """
         if (type(size) != int):
             raise TypeError("size must be an integer")
@@ -21,21 +19,17 @@ class Square:
         else:
             self.size = size
 
-
     @property
     def size(self):
         """A getter(accessor) method for private instance attribute: size.
-
         Returns:
             The return value is the private attribute size.
         """
         return self.__size
 
-
     @size.setter
     def size(self, value):
         """A setter(mutator) method for private instance attribute: size.
-
         Args:
             value (int): The size of the square.
         """
@@ -46,16 +40,12 @@ class Square:
         else:
             self.__size = value
 
-
     def area(self):
         """A public instance menthod
-
         Returns:
             The return value is the area of a square
-
         """
         return self.__size**2
-
 
     def my_print(self):
         """Prints in stdout the square with the character #"""
