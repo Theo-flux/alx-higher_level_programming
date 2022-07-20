0904447dd3832707bc6e24fd09f296589f4f52f7
Theo-flux <ekunnusitheophilus@gmail.com>
2022-07-20 09:15:59 +0000

    101-square fix

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -60,11 +60,8 @@ class Square:
 
     def __str__(self):
         """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
