27460115925a6149266adf0992f86db5dc312b61
Theo-flux <ekunnusitheophilus@gmail.com>
2022-07-20 09:48:22 +0000

    task-102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,88 +1,55 @@
 #!/usr/bin/python3
 
-'''Define a square class'''
+"""Define a class Square."""
+
 
 class Square:
-    '''Representation of a square'''
+    """Represent a square."""
 
     def __init__(self, size=0):
-        '''Instatiation'''
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
     def size(self):
-        '''Property: to retrieve data.
-
-        Returns:
-            To return data
-        '''
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        '''property setter: to set it
-
-        Args:
-            value (int): value for data
-        '''
-        if (value < 0):
-            raise ValueError('size must be >= 0')
-        elif (not isinstance(value, int)):
-            return TypeError('size must be a number')
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        '''Public instance method
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-        Returns:
-            Area of current square
-        '''
-        return self.__size**2
-
-    def __et__(self, other):
-        '''equality comparator.
-
-        Args:
-            other (Square): instance of Square Type.
-        '''
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
         return self.area() == other.area()
 
     def __ne__(self, other):
-        '''not equal comparator
-
-        Args:
-            other (Square): instance of Square Type.
-        '''
+        """Define the != comparison to a Square."""
         return self.area() != other.area()
 
-    def __gt__(self, other):
-        '''greater than comparator
-
-        Args:
-            other (Square): instance of Square Type.
-        '''
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        '''greater than or equal comparator
-
-        Args:
-            other (Square): instance of Square Type.
-        '''
-        return self.area() >= other.area()
-
     def __lt__(self, other):
-        '''less than comparator
-
-        Args:
-            other (Square): instance of Square Type.
-        '''
+        """Define the < comparison to a Square."""
         return self.area() < other.area()
 
     def __le__(self, other):
-        '''less than comparator
-
-        Args:
-            other (Square): instance of Square Type.
-        '''
+        """Define the <= comparison to a Square."""
         return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
