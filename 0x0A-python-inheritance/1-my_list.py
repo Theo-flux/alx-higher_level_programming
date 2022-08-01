2f8e9c39e89bf1464266cb89c75db6cdb5a3d463
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-08-01 14:09:54 +0100

    test cases added

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,9 +2,10 @@
 """Define MyList class"""
 
 class MyList(list):
-    """A representation of MyList data type"""
-    def print_sorted(self):
-        """Public instance method that prints the
-        list in ascending order
-        """
-        print(sorted(self))
+	"""A representation of MyList data type"""
+    
+	def print_sorted(self):
+        	"""Public instance method that prints the
+        	list in ascending order
+        	"""
+        	print(sorted(self))
