c41ee9aeb79f762e42ab4d9111f7fd4d5113a547
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-08-01 13:50:13 +0100

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define MyList class"""
+
+class MyList(list):
+    """A representation of MyList data type"""
+    def print_sorted(self):
+        """Public instance method that prints the
+        list in ascending order
+        """
+        print(sorted(self))
