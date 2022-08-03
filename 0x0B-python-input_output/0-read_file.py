5bdc2b08a65f2dba6e26c722ff56836d7ebec8d7
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-08-03 04:25:55 +0100

    Write to a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,11 @@
 
 
 def read_file(filename=""):
+	"""function to read from a file
+	
+	Args:
+	     filename (str): name of the file
+	"""
 	with open(filename, mode="r", encoding="UTF-8") as f:
 		text = f.read()
 		print(text)
