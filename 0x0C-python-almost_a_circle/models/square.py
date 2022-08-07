cb186ad32ed251a0b937ddbfb657159833e67b0a
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-08-07 16:24:46 +0100

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Define Square class
+"""
+
+
+from models.rectangle import Rectangle
+class Square(Rectangle):
+	""" A represntation of Square
+	"""
+	def __init__(self, size, x=0, y=0, id=None):
+		""" Class constructor
+		
+		Args:
+		     x (int): x coordinate
+		     y (int): y coordinate
+		     size (int): length of square
+		     id (int): identity for square instance
+		"""
+		super().__init__(size, size, x, y, id)
+		self.size = size
+	
+	def __str__(self):
+		return "[{}] ({}) {}/{} - {}"\
+			.format(self.__class__.__name__,\
+			self.id, self.x, self.y, self.size)
