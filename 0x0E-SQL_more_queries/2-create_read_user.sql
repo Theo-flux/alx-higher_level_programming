fb146e6f9a0cad750e157ee4f9ae03c2c5eb35b3
Theo <ekunnusitheophilus@gmail.com>
2022-09-07 17:02:28 +0100

    2. Read user (Hot fix)

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Creates the database `hbtn_0c_0` and the user `user_0d_2`.
+-- Creates the database `hbtn_0d_2` and the user `user_0d_2`.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT PRIVILEGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
