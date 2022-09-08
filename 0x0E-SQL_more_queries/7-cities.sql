f2797ecee7b9d45be51345985547be156e21d086
Theo <ekunnusitheophilus@gmail.com>
2022-09-08 03:06:41 +0100

    7. Cities (Hot fix)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,10 @@
--- Create database `hbtn_0d_usa` and the table `cities` on your mysql server
+-- Creates the database hbtn_0d_usa with the table cities.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-`state_id` INT NOT NULL FOREGIN KEY REFERENCES `hbtn_0d_usa`.`states`(`id`),
-`name` VARCHAR(256) NOT NULL
+    PRIMARY KEY(`id`),
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `state_id` INT          NOT NULL,
+    `name`     VARCHAR(256) NOT NULL,
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
