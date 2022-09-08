d0100f70fe5cabb534f7f994fba7e55146a9d3df
Theo <ekunnusitheophilus@gmail.com>
2022-09-08 11:36:29 +0100

    18. No Comedy tonight! (Hot Fix)

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,14 +1,16 @@
 -- List all show without the genre  `Comedy` int the database `hbtn_0d_tvshows`
-SELECT DISTINCT(ts.`title`) FROM `tv_shows` AS ts
+SELECT DISTINCT `title`
+FROM `tv_shows` AS ts
 LEFT JOIN `tv_show_genres` AS tsg
-ON ts.`id` = tsg.`show_id`
+ON tsg.`show_id` = ts.`id`
 LEFT JOIN `tv_genres` AS tg
 ON tg.`id` = tsg.`genre_id`
-WHERE ts.`title` NOT IN (
-SELECT ts.`title` FROM `tv_shows` AS ts
-INNER JOIN `tv_show_genres` AS tsg
-ON ts.`id` = tsg.`genre_id`
-INNER JOIN `tv_genres` AS tg
-ON tg.`id` = tsg.`show_id`
-WHERE tg.`name` = 'Comedy')
-ORDER BY ts.`title`;
+WHERE ts.`title` NOT IN
+(SELECT `title`
+  FROM `tv_shows` AS ts
+  INNER JOIN `tv_show_genres` AS tsg
+  ON tsg.`show_id` = ts.`id`
+  INNER JOIN `tv_genres` AS tg
+  ON tg.`id` = tsg.`genre_id`
+  WHERE tg.`name` = "Comedy")
+ORDER BY `title`;
