c5f196d3b9acd98575f9130e91d14b49b6ad2ef0
Theo <ekunnusitheophilus@gmail.com>
2022-10-04 19:53:53 +0100

    Readme fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,12 @@
 const Squrae5 = require('./5-square');
 
 class Square extends Squrae5 {
-  constructor (size) {
-    super();
+  constructor(size) {
+    super(size);
     this.size = size;
   }
 
-  charPrint (c) {
+  charPrint(c) {
     if (c) {
       for (let i = 0; i < this.size; i++) {
         console.log('C'.repeat(this.size));
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -1,1 +1,3 @@
 0x13. JavaScript - Objects, Scopes and Closures
+
+[objects basics][https://alx-intranet.hbtn.io/rltoken/dsskbb-cj0tqufl8eozllq]
