0b57d53909fea7a9ff6d3a640bda5656bae9304b
Theo <ekunnusitheophilus@gmail.com>
2022-10-04 19:30:54 +0100

    6. Square #6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const Squrae5 = require('./5-square');
+
+class Square extends Squrae5 {
+  constructor (size) {
+    super();
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.size; i++) {
+        console.log('C'.repeat(this.size));
+      }
+    }
+
+    for (let i = 0; i < this.size; i++) {
+      console.log('X'.repeat(this.size));
+    }
+  }
+}
+
+module.exports = Square;
