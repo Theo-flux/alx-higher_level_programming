f533843d4e14ae1abed0e472bac1ff6065faa390
Theo <ekunnusitheophilus@gmail.com>
2022-10-25 05:20:36 +0100

    making request with Request module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+
+
+if __name__ == '__main__':
+    import requests
+
+    with requests.get('https://alx-intranet.hbtn.io/status') as res:
+        print('Body response:')
+        print("\t- type:", type(res.text))
+        print("\t- type:", res.text)
