df63fc8c895cdc993c654e2fdd81b05d4cbd0f8a
Theo <ekunnusitheophilus@gmail.com>
2022-10-25 05:01:08 +0100

    Hot Fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
 
     try:
         with request.Request(url) as res:
-            print(res.read().decode('UTF-8'))
+            print(res.read().decode('utf-8'))
     except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        print("Error code:", e.code)
