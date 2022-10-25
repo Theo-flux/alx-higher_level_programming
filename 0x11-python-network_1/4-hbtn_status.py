ed4380856d90dfca315585ac70f7033c69d74937
Theo <ekunnusitheophilus@gmail.com>
2022-10-25 05:24:50 +0100

    Hot Fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@
 if __name__ == '__main__':
     import requests
 
-    with requests.get('https://alx-intranet.hbtn.io/status') as res:
-        print('Body response:')
-        print("\t- type:", type(res.text))
-        print("\t- type:", res.text)
+    re = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type:", type(res.text))
+    print("\t- type:", res.text)
