ce1104cb50f54dcdeb8048b848c226d21fd158bd
Theo <ekunnusitheophilus@gmail.com>
2022-10-25 04:28:42 +0100

    Hot Fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" display the value of the X-Request-Id variable found in the header of the response. """
+"""
+display the value of the X-Request-Id variable
+found in the header of the response.
+"""
 
 
 if __name__ == '__main__':
