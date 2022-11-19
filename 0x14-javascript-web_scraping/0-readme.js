fe0350992628f1678e5a12ac741d478f48993c93
Theo <ekunnusitheophilus@gmail.com>
2022-11-19 07:53:41 +0100

    Hot Fix with semistandard.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,7 @@
 const fs = require('fs');
 const process = require('process');
 
-const path = __dirname;
-console.log(path);
-fs.readFile(`${path}/${process.argv[2]}`, 'utf8', (err, data) => {
+fs.readFile(`${process.argv[2]}`, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
     return;
