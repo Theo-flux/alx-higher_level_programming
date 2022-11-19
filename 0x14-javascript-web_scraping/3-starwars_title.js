36e33f1d9a99d043b2a459f1bb40377b370481bf
Theo <ekunnusitheophilus@gmail.com>
2022-11-19 08:57:04 +0100

    Hot Fix.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,15 +3,15 @@
 
 const request = require('request');
 const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+const API_URL = `https://swapi-api.hbtn.io/api/films/${episodeNum}`;
 
-request(API_URL + episodeNum, function (err, response, body) {
+request(API_URL, function (err, res, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
+  } else if (res.statusCode === 200) {
+    const bodyJSON = JSON.parse(body);
+    console.log(bodyJSON.title);
   } else {
-    console.log('Error code: ' + response.statusCode);
+    console.log(`code: ${res.statusCode}`);
   }
 });
