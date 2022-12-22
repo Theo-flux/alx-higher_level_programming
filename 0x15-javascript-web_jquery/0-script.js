a9f2cfef51665e2a60af8407ce2e646e62b1b785
Theo <ekunnusitheophilus@gmail.com>
2022-12-22 16:20:34 +0100

    [feat.] jquery click event.

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,6 +1,6 @@
 // a JavaScript script that updates the text color of
 // the <header> element to red (#FF0000):
 
-const headerEl = document.querySelector('header');
+const headerEl = document.querySelector('HEADER');
 
 headerEl.style.color = '#FF0000';
