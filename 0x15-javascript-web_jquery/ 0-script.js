0305a128ede0afffb914d84930d53dde7a8a71fd
Theo <ekunnusitheophilus@gmail.com>
2022-12-22 15:44:54 +0100

    [ref] script to update text color.

---
+++ 0x15-javascript-web_jquery/ 0-script.js
@@ -0,0 +1,6 @@
+// a JavaScript script that updates the text color of
+// the <header> element to red (#FF0000):
+
+const headerEl = document.querySelector('header');
+
+headerEl.style.color = '#FF0000';
