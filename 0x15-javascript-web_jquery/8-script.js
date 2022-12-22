d99875a75f1b570529ce9715f2a4e7b70adb3c90
Theo <ekunnusitheophilus@gmail.com>
2022-12-22 17:48:16 +0100

    [code] script - 9 jquery:ajax:get

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,13 +1,11 @@
 $('document').ready(() => {
-    $.ajax(
-        {
-            url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
-            success: (res) => {
-                const { results } = res;
-                results.map((data) => {
-                    $('UL#list_movies').append(`<li>${data.title}</li>`)
-                })
-            }
-        }
+    $.ajax({
+        url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+        success: (res) => {
+            const { results } = res;
+            results.map((data) => {
+                $('UL#list_movies').append(`<li>${data.title}</li>`)
+            })
+        }}
     );
 });
