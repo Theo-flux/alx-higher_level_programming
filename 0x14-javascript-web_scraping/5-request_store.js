c0bbef4c22d45bdc443d92514d4bc75326e86bcb
Theo <ekunnusitheophilus@gmail.com>
2022-12-28 09:26:29 +0100

    [fix] 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const { writeFileSync } = require('fs');
+const fs = require('fs');
 
 request(process.argv[2], function (err, response, body) {
   if (err == null) {
-    writeFileSync(process.argv[3], body, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    fs.writeFileSync(process.argv[3], body);
   }
 });
